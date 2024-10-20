c8eeffffdbb95af557f621e5886e030ccc011b2a
abhilash <abhilashv8877@gmail.comgit config --global user.email abhilashv8877@gmail.com>
2024-10-21 00:17:36 +0530

    validation done

--- src/coding.py
+++ src/coding.py
@@ -322,29 +322,33 @@ def registerCanteen():
 
 @app.route("/canteen_register_code", methods=['post'])
 def canteen_register_code():
-    name = request.form["textfield"]
-    place = request.form["textfield2"]
-    post = request.form["textfield3"]
-    pin = request.form["textfield4"]
-    phone = request.form["textfield5"]
-    email = request.form["textfield6"]
-    license = request.files["textfield7"]
-    username = request.form["textfield8"]
-    password = request.form["textfield9"]
 
-    license_name = secure_filename(license.filename)
-    license.save(os.path.join("static/uploads", license_name))
+    try:
+        name = request.form["textfield"]
+        place = request.form["textfield2"]
+        post = request.form["textfield3"]
+        pin = request.form["textfield4"]
+        phone = request.form["textfield5"]
+        email = request.form["textfield6"]
+        license = request.files["textfield7"]
+        username = request.form["textfield8"]
+        password = request.form["textfield9"]
 
-    qry = "INSERT INTO`login` VALUES(NULL,%s,%s,'pending')"
-    val = (username, password)
-    id = iud(qry, val)
+        license_name = secure_filename(license.filename)
+        license.save(os.path.join("static/uploads", license_name))
 
-    qry = "INSERT INTO `canteen` VALUES(NULL,%s,%s,%s,%s,%s,%s,%s, %s)"
-    val = (id, name, place, post, pin, phone, email, license_name)
+        qry = "INSERT INTO`login` VALUES(NULL,%s,%s,'pending')"
+        val = (username, password)
+        id = iud(qry, val)
 
-    iud(qry,val)
-    return '''<script>alert("Registration partially completed....wait for verification");window.location="/"</script>'''
+        qry = "INSERT INTO `canteen` VALUES(NULL,%s,%s,%s,%s,%s,%s,%s, %s)"
+        val = (id, name, place, post, pin, phone, email, license_name)
 
+        iud(qry,val)
+        return '''<script>alert("Registration partially completed....wait for verification");window.location="/"</script>'''
+    except Exception as e:
+        print(e)
+        return'''<script>alert("Email or phone already exists");window.location="/"</script>'''
 
 
 @app.route("/manage_user")
@@ -557,30 +561,38 @@ def userRegistration():
 
 @app.route("/user_register_code",methods=['post'])
 def user_register_code():
-    canteen_id = request.form["select"]
-    name = request.form["textfield"]
-    id_dtails = request.files["file"]
-    email = request.form["textfield2"]
-    phone = request.form["textfield3"]
-    profile_photo = request.files["file2"]
-    username = request.form["textfield4"]
-    password = request.form["textfield5"]
 
-    id_details_name = secure_filename(id_dtails.filename)
-    id_dtails.save(os.path.join("static/uploads", id_details_name))
+    try:
+        canteen_id = request.form["select"]
+        name = request.form["textfield"]
+        id_dtails = request.files["file"]
+        email = request.form["textfield2"]
+        phone = request.form["textfield3"]
+        profile_photo = request.files["file2"]
+        username = request.form["textfield4"]
+        password = request.form["textfield5"]
 
-    profile_photo_name = secure_filename(profile_photo.filename)
-    profile_photo.save(os.path.join("static/uploads", profile_photo_name))
+        id_details_name = secure_filename(id_dtails.filename)
+        id_dtails.save(os.path.join("static/uploads", id_details_name))
 
-    qry = "INSERT INTO`login` VALUES(NULL,%s,%s,'pending')"
-    val = (username, password)
-    id = iud(qry, val)
+        profile_photo_name = secure_filename(profile_photo.filename)
+        profile_photo.save(os.path.join("static/uploads", profile_photo_name))
 
-    qry = "INSERT INTO `user` VALUES(NULL,%s,%s,%s,%s,%s,%s,%s)"
-    val = (id, canteen_id, name, id_details_name, email, phone, profile_photo_name)
+        qry = "INSERT INTO`login` VALUES(NULL,%s,%s,'pending')"
+        val = (username, password)
+        id = iud(qry, val)
 
-    iud(qry, val)
-    return '''<script>alert("Registration partially completed...wait for verification");window.location="/"</script>'''
+        qry = "INSERT INTO `user` VALUES(NULL,%s,%s,%s,%s,%s,%s,%s)"
+        val = (id, canteen_id, name, id_details_name, email, phone, profile_photo_name)
+
+        iud(qry, val)
+        return '''<script>alert("Registration partially completed...wait for verification");window.location="/"</script>'''
+    except Exception as e:
+         print(e)
+         return'''<script>alert("Email or phone already exists");window.location="/"</script>'''
+
+
+
 
 
 @app.route("/recharge_wallet")
